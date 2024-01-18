0f78b3acf8e7145f3408fd3f41dd13eb3f24522b
Fiw <wigingtai@gmail.com>
2024-01-18 22:56:38 +0700

    build up the page from idea

---
+++ script.js
@@ -0,0 +1,47 @@
+
+function showSmallRoom() {
+    document.querySelector('.banner').style.backgroundImage = 'url("img/smallRoom-banner.jpg")';
+    document.querySelector('.head').style.opacity = '0';
+    document.querySelector('.text').style.opacity = '0';
+}
+
+function hideSmallRoom() {
+    // Reset the background image
+    document.querySelector('.banner').style.backgroundImage = 'url("img/background-banner.jpg")';
+    // Show the elements with the classes 'head' and 'text'
+    document.querySelector('.head').style.opacity = '1';
+    document.querySelector('.text').style.opacity = '1';
+}
+
+function showMidRoom() {
+    document.querySelector('.banner').style.backgroundImage = 'url("img/midRoom-banner.jpg")';
+    document.querySelector('.head').style.opacity = '0';
+    document.querySelector('.text').style.opacity = '0';
+}
+
+function hideMidRoom() {
+    // Reset the background image
+    document.querySelector('.banner').style.backgroundImage = 'url("img/background-banner.jpg")';
+    // Show the elements with the classes 'head' and 'text'
+    document.querySelector('.head').style.opacity = '1';
+    document.querySelector('.text').style.opacity = '1';
+}
+
+function showLargeRoom() {
+    document.querySelector('.banner').style.backgroundImage = 'url("img/largeRoom-banner.jpg")';
+    document.querySelector('.head').style.opacity = '0';
+    document.querySelector('.text').style.opacity = '0';
+}
+
+function hideLargeRoom() {
+    // Reset the background image
+    document.querySelector('.banner').style.backgroundImage = 'url("img/background-banner.jpg")';
+    // Show the elements with the classes 'head' and 'text'
+    document.querySelector('.head').style.opacity = '1';
+    document.querySelector('.text').style.opacity = '1';
+}
+
+function goToPage() {
+    window.location.href = "https://www.facebook.com/profile.php?id=100057029126631";
+
+}
